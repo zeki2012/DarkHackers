51768d64228d0ae5e60198abba54444b966d1001
= <=>
2023-12-12 22:36:01 +0400

    +1

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-let hesabla = document.getElementById('hesab');
+let hesab = document.getElementById('hesab');
 let bmi;
+let netice;
 
-hesabla.onclick = () => {
-    bmi = (ceki.value / (boy.value ** 2)) * 10000 ;
+let bmiMore;
+let bmiLess;
 
-if (bmi < 18) {
-    netice = 'Siz çəki azlığından əziyyət çəkirsiniz.';
-} else if (bmi < 20) {
-  netice = 'Sizin, çəkiniz azdır. Lakin sağlamlıq üçün zərərli deyil.';
-} else if (bmi < 26) {
-  netice = 'Sizin, çəkiniz normaldır. Təbriklər !!!';
-} else if (bmi < 28) {
-  netice = 'Sizin artıq çəkiniz var.';
-} else if (bmi < 31) {
-  netice = 'Siz, 1-ci dərəcəli artıq çəkidən əziyyət çəkirsiniz.';
-} else if (bmi < 36) {
-  netice = 'Siz, 2-ci dərəcəli artıq çəkidən əziyyət çəkirsiniz.';
-} else if (bmi < 41) {
-  netice = 'Siz, 3-cü dərəcəli artıq çəkidən əziyyət çəkirsiniz.';
-} else {
-  netice = 'Siz, 4-cü dərəcəli artıq çəkidən əziyyət çəkirsiniz.';
-}
+let resultCox;
+let resultAz;
 
+let azCox;
 
-    document.getElementById('zeki').innerHTML = netice
+hesab.onclick = () => {
+    bmi = (ceki.value / (boy.value ** 2)) * 10000;
+    bmiMore = (26 * (boy.value ** 2)) / 10000;
+    bmiLess = (20 * (boy.value ** 2)) / 10000;
+    resultCox = ceki.value - bmiMore;
+    resultAz = bmiLess - ceki.value;
+
+    if (bmi < 20) {
+        azCox = 'az';
+    } else {
+        azCox = 'artıq';
+    }
+
+    if (bmi < 18) {
+        netice = `Siz çəki azlığından əziyyət çəkirsiniz. ${resultAz.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi < 20) {
+        netice = `Sizin çəkiniz azdır. Lakin sağlamlıq üçün zərəli deyil. ${resultAz.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi < 26) {
+        netice = 'Sizin çəkiniz normaldır. Təbriklər';
+    } else if (bmi < 28) {
+        netice = `Sizin artıq çəkiniz var. ${resultCox.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi < 31) {
+        netice = `Siz 1-ci dərəcəli artıq çəkidən əziyyət çəkirsiniz. ${resultCox.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi < 36) {
+        netice = `Siz 2-ci dərəcəli artıq çəkidən əziyyət çəkirsiniz. ${resultCox.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi < 41) {
+        netice = `Siz 3-cü dərəcəli artıq çəkidən əziyyət çəkirsiniz. ${resultCox.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else if (bmi >= 41) {
+        netice = `Siz 4-cü dərəcəli artıq çəkidən əziyyət çəkirsini. ${resultCox.toFixed(2)} kq ${azCox} cekiniz var`;
+    } else {
+        netice = 'Xanaları düzgün doldurun';
+    }
+    document.getElementById('zeki').innerHTML = netice;
 }
